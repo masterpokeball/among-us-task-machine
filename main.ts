0fdaa21b90dc6aa8118b427229ce341d348aa6d1
masterpokeball <67118920+masterpokeball@users.noreply.github.com>
2022-02-06 08:01:19 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 radio.onReceivedValue(function (name, value) {
-    if (name == "real" && value >= 2) {
+    if (name == "fake" && value == 1) {
         basic.pause(5000)
-        basic.showString("Done!")
-    } else if (name == "fake" && value == 1) {
+        basic.showString("Task faked!")
+    } else if (name == "real" && value >= 2) {
         basic.pause(5000)
-        basic.showString("Done.")
+        basic.showString("Task finished!")
     }
 })
 radio.setGroup(7)
 basic.forever(function () {
-    basic.showString("TASK!")
+    basic.showString("TASKS!")
 })
