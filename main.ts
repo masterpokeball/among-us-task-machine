58522151898b304910a1ac7e1264924eeca37db6
Chase J <67118920+masterpokeball@users.noreply.github.com>
2022-02-12 16:30:13 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,7 @@
 radio.onReceivedValue(function (name, value) {
     if (input.buttonIsPressed(Button.AB) && (name == "fake" && value == 1)) {
-        basic.pause(4000)
         basic.showString("Task faked!")
     } else if (input.buttonIsPressed(Button.AB) && (name == "real" && value >= 2)) {
-        basic.pause(4000)
         basic.showString("Task finished!")
     }
 })
